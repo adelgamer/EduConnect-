ca621d37b04ce3d10ea2c371652ea9aabb431c25
Adel Amrane <62817038+adelgamer@users.noreply.github.com>
2025-12-27 18:32:16 +0100

    Set up swagger docs

--- app.ts
+++ app.ts
@@ -4,6 +4,9 @@ import mainRouter from './src/mainRouter.js';
 import { baseMiddleware } from './src/middlewares/base.middleware.js';
 import { globalErrorHandler } from './core/middlewares/globalErrorHandler.js';
 import cookieParser from 'cookie-parser';
+import { Options } from 'swagger-jsdoc';
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 
 const app = express();
 const port = 3001;
@@ -16,10 +19,31 @@ app.use(baseMiddleware);
 // Routes
 app.use(mainRouter);
 
-
 // Error handlers
 app.use(globalErrorHandler);
 
+// Swagger
+export const swaggerOptions: Options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: process.env.APP_NAME ?? 'New App',
+            version: process.env.APP_VERSION ?? '1.0.0',
+            description: process.env.APP_DESCRIPTION ?? 'New App',
+        },
+        servers: [
+            {
+                url: `http://localhost:${port}`,
+            },
+        ],
+    },
+    // Path to the API docs (where your route files are)
+    apis: ['./src/modules/*.ts'],
+};
+const specs = swaggerJsdoc(swaggerOptions);
+app.use(process.env.SWAGGER_ENDPOINT ?? '/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+
+
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port} \nSwagger docs on http://localhost:${port}${process.env.SWAGGER_ENDPOINT ?? '/api-docs'}`);
 });
