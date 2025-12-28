cf119d3812f3b253665d89e144da5d5e94e09971
Adel Amrane <62817038+adelgamer@users.noreply.github.com>
2025-12-28 19:52:54 +0100

    Added Fetch a single post

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -13,7 +13,12 @@ export async function getAll() {
  * Fetches a single Post by ID.
  */
 export async function getById(id: string) {
-    return `Post with ID: \${id\} retrieved from the service.`;
+    // 1- Checking if post exists and not deleted
+    const post = await prisma.post.findUnique({ where: { id } });
+    if (!post || (post && post.isDeleted)) throw new NotFoundExcpetion('Post not found');
+
+    // 2- return the post
+    return post;
 }
 
 /**
@@ -29,17 +34,14 @@ export async function create(userId: string, data: any) {
  * Processes data to update an existing Post.
  */
 export async function update(actorId: string, id: string, data: any) {
-    // 1- Checking if post exists
+    // 1- Checking if post exists and not deleted
     const post = await prisma.post.findUnique({ where: { id } });
-    if (!post) throw new NotFoundExcpetion('Post not found');
+    if (!post || (post && post.isDeleted)) throw new NotFoundExcpetion('Post not found');
 
     // 2- Checking if user owns the post
     if (post.userId !== actorId) throw new UnauthorizedExcpetion('You don\'t have permission to edit this post');
 
-    // 3- Check if post alread deleted
-    if (post.isDeleted) throw new NotFoundExcpetion('Post not found');
-
-    // 4- Updating the post
+    // 3- Updating the post
     const updatedPost = await prisma.post.update({
         where: { id },
         data
@@ -52,17 +54,14 @@ export async function update(actorId: string, id: string, data: any) {
  * Performs the deletion of a Post record.
  */
 export async function remove(actorId: string, id: string) {
-    // 1- Checking if post exists
+    // 1- Checking if post exists and not deleted
     const post = await prisma.post.findUnique({ where: { id: id } });
-    if (!post) throw new NotFoundExcpetion('Post not Found');
+    if (!post || (post && post.isDeleted)) throw new NotFoundExcpetion('Post not found');
 
     // 2- Checking if user owns the post
     if (actorId !== post.userId) throw new UnauthorizedExcpetion('You don\'t have permission to edit this post');
 
-    // 3- Check if post alread deleted
-    if (post.isDeleted) throw new NotFoundExcpetion('Post not found');
-
-    // 4- Soft deleting post
+    // 3- Soft deleting post
     await prisma.post.update({ where: { id }, data: { isDeleted: true } });
     post.isDeleted = true;
 
